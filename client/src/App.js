7099b0246badb073743218a5b3cbbc6733e255f4
Nguyen Cong Thanh <thanhnc138@gmail.com>
2022-11-16 23:40:53 +0700

    feat: add bar chart visualization for footfall data

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,50 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const barChartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Footfall Visualization',
+    },
+  },
+};
+
 function App() {
-  return <div className="App">Hello</div>;
+  const [barChartData, setBarChartData] = useState({ labels: [], datasets: [] });
+  const loadData = async () => {
+    const response = await axios.get('http://localhost:3001/footfall');
+    if (response.status === 200) {
+      const footfallData = response.data.data;
+      const labels = footfallData.map((footfallItem) => footfallItem.Time.slice(0, 10));
+      const datasetItem = {
+        label: 'Number of People by Day',
+        data: footfallData.map((footfallItem) => footfallItem.Value),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      };
+      setBarChartData({
+        labels,
+        datasets: [datasetItem],
+      });
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+  return (
+    <div className="App">
+      <Bar options={barChartOptions} data={barChartData} />
+    </div>
+  );
 }
 
 export default App;
